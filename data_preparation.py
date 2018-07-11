d95d1d8af34259b24348f3358a2166bd43b85c3f
vittoboa <boarinivittorio@gmail.com>
2018-07-11 16:47:25 +0200

    Remove url links from tweets text

--- data_preparation.py
+++ data_preparation.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 import constants as K
 
@@ -7,9 +8,14 @@ def remove_multiple_whitespaces(text):
     return " ".join(text.split())
 
 
+def remove_urls(text):
+    return re.sub(r'http\S+', '', text)
+
+
 if __name__ == '__main__':
     with open(K.FETCHED_TWEETS_FILE, newline='') as tweets_f:
         reader = csv.DictReader(tweets_f)
         for row in reader:
             text = row[K.FIELDNAMES[1]]
             text_cleaned = remove_multiple_whitespaces(text)
+            text_cleaned = remove_urls(text_cleaned)
