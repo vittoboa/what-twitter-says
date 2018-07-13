72805b138e3ba7557b3f09c0711b09ae9888cdf9
vittoboa <boarinivittorio@gmail.com>
2018-07-13 20:31:57 +0200

    Make tweets text lowercase

--- data_preparation.py
+++ data_preparation.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
         for row in reader:
             text = row[K.FIELDNAMES[1]]
             text = remove_non_alphanumeric(text)
+            text = text.lower()
             print(text)
