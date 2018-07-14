d0eeb54892d144c09eab2709043a902971922505
vittoboa <boarinivittorio@gmail.com>
2018-07-14 13:42:25 +0200

    [cosmetic] Add comments to explain the process

--- data_preparation.py
+++ data_preparation.py
@@ -12,11 +12,19 @@ if __name__ == '__main__':
     with open(K.FETCHED_TWEETS_FILE, 'r', newline='') as f_input, open(K.CLEANED_TWEETS_FILE, 'w') as f_output:
         reader = csv.DictReader(f_input)
         writer = csv.DictWriter(f_output, fieldnames=K.FIELDNAMES)
+
+        # write the column names in the first row
         writer.writeheader()
+        
         for row in reader:
+            # get raw time and text
             time, text = row[K.FIELDNAMES[0]], row[K.FIELDNAMES[1]]
+            
+            # clean text
             text = remove_non_alphanumeric(text)
             text = text.lower()
+
+            # store time and text in the file
             writer.writerow({
                 K.FIELDNAMES[0]: time,
                 K.FIELDNAMES[1]: text
