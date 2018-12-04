57faf382e87b764c571043b2bd647cf37c9105ea
vittoboa <boarinivittorio@gmail.com>
2018-12-04 13:25:05 +0100

    Refactor Input/Output

--- data_preparation.py
+++ data_preparation.py
@@ -21,17 +21,14 @@ def remove_stopwords(text):
 if __name__ == '__main__':
     stopwords = stopwords.words("english")
 
-    with open(K.FETCHED_TWEETS_FILE, 'r', newline='') as f_input, open(K.CLEANED_TWEETS_FILE, 'w') as f_output:
-        reader = csv.DictReader(f_input)
-        writer = csv.DictWriter(f_output, fieldnames=K.FIELDNAMES)
-
-        # write the column names in the first row
-        writer.writeheader()
+    with open(K.FETCHED_TWEETS_FILE, 'r') as f_input, open(K.CLEANED_TWEETS_FILE, 'w') as f_output:
+        reader = csv.reader(f_input)
+        writer = csv.writer(f_output)
         
-        reader_sorted_by_time = sorted(reader, key=lambda row:row[K.FIELDNAMES[0]])
+        reader_sorted_by_time = sorted(reader, key=lambda row:row[0])
         for row in reader_sorted_by_time:
             # get raw time and text
-            time, text = row[K.FIELDNAMES[0]], row[K.FIELDNAMES[1]]
+            time, text = row
 
             # clean time
             time = remove_seconds(time)
@@ -42,7 +39,4 @@ if __name__ == '__main__':
             text = remove_stopwords(text)
 
             # store time and text in the file
-            writer.writerow({
-                K.FIELDNAMES[0]: time,
-                K.FIELDNAMES[1]: text
-            })
+            writer.writerow((time, text))
