1c34623e9e7220b5e22734d2bb8f2737f4395cfe
vittoboa <boarinivittorio@gmail.com>
2018-12-06 22:54:47 +0100

    Replace slang words

--- constants.py
+++ constants.py
@@ -9,3 +9,14 @@ FETCHED_TWEETS_FILE   = "raw_tweets.csv"
 CLEANED_TWEETS_FILE   = "cleaned_tweets.csv"
 SENTIMENT_TWEETS_FILE = "sentiment_tweets.csv"
 FIELDNAMES = ["time", "text"]
+SLANG = {
+    "ftw": "for the win",
+    "gr8": "great",
+    "imo": "in my opinion",
+    "lol": "laughing out loud",
+    "smh": "disappointed",  # Shaking My Head in disappointment
+    "smdh": "very disappointed",  # Shaking My Damn Head
+    "btw": "by the way",
+    "b4": "before",
+    "idk": "i don't know"
+}
--- data_preparation.py
+++ data_preparation.py
@@ -18,25 +18,34 @@ def remove_stopwords(text):
     return " ".join(word for word in text.split() if word not in stopwords)
 
 
+def replace_slang(text):
+    return " ".join(K.SLANG.get(word, word) for word in text.split())
+
+
+def clean_text(text):
+    text = remove_non_alphanumeric(text)
+    text = text.lower()
+    text = replace_slang(text)
+    text = remove_stopwords(text)
+
+    return text
+
+
+def clean_time(time):
+    time = remove_seconds(time)
+
+    return time
+
+
 if __name__ == '__main__':
     stopwords = stopwords.words("english")
 
     with open(K.FETCHED_TWEETS_FILE, 'r') as f_input, open(K.CLEANED_TWEETS_FILE, 'w') as f_output:
         reader = csv.reader(f_input)
         writer = csv.writer(f_output)
-        
+
         reader_sorted_by_time = sorted(reader, key=lambda row:row[0])
-        for row in reader_sorted_by_time:
-            # get raw time and text
-            time, text = row
-
-            # clean time
-            time = remove_seconds(time)
-
-            # clean text
-            text = remove_non_alphanumeric(text)
-            text = text.lower()
-            text = remove_stopwords(text)
-
-            # store time and text in the file
+        for time, text in reader_sorted_by_time:
+            time = clean_time(time)
+            text = clean_text(text)
             writer.writerow((time, text))
